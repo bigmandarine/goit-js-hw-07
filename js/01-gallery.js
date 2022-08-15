c267d2ef8afd3a0d0f8e124ec31389a5dba95cea
Dima Kuzko <bigmandarine1991@gmail.com>
2022-08-15 14:21:28 +0300

    Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const cardsGalleryMarkup = createMarkupGallery(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', cardsGalleryMarkup);
+
+function createMarkupGallery(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}"/>
+      </a>
+      </div>`;
+    })
+    .join('');
+}
+
+galleryEl.addEventListener('click', modalGallery);
+const instance = basicLightbox.create(`<img class="modal-img" src="">`, {
+  onShow: instance => {
+    window.addEventListener('keydown', onEscPress);
+  },
+
+  onClose: instance => {
+    window.removeEventListener('keydown', onEscPress);
+  },
+});
+function modalGallery(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  instance.element().querySelector('img').src = evt.target.dataset.source;
+  instance.show();
+}
+function onEscPress(evt) {
+  if (evt.code === 'Escape') {
+    instance.close();
+    return;
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const cardsGalleryMarkup = createMarkupGallery(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', cardsGalleryMarkup);
 
-console.log(galleryItems);
+function createMarkupGallery(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+      </a>
+      `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
